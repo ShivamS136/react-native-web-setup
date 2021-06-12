d732e1dab375024dad770454dab68b500a9bd78d
Shivam Sharma <shivamshrm235@gmail.com>
2021-06-12 21:03:26 +0530

    Web Support added in React Native App

---
+++ index.web.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import App from './src/components/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.runApplication(appName, {
+  rootTag: document.getElementById('react-native-web-app'),
+});
